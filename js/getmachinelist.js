cf0ee929719a625e7320d5a6fbf0a3c786425e54
a96009467 <a96009467@gmail.com>
2020-02-18 15:55:13 +0800

    Update getmachinelist.js

--- js/getmachinelist.js
+++ js/getmachinelist.js
@@ -5,7 +5,7 @@ $(document).ready(function($) {
 });
 
 $.ajax({
-    url: "https://192.168.2.120:443/getmachinelist", //127.0.0.1:8091
+    url: "http://192.168.2.120:8091/getmachinelist", //127.0.0.1:8091
     type: 'POST',								    //方法用POST取回
     data: {},									    //參數
     cache : false,								    //為了防止ajax使用緩存
